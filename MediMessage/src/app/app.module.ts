ff6a1448b95ea93dc18968287b39a1a7c9e0e82c
Amrut Kulkarni <akulkarni74@gatech.edu>
2018-07-05 19:36:30 -0400

    Still working through Angular parts of Udemy Course

--- MediMessage/src/app/app.module.ts
+++ MediMessage/src/app/app.module.ts
@@ -4,20 +4,33 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
-import {MatInputModule, MatCardModule} from '@angular/material';
+import {
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatExpansionModule
+      } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     MatInputModule,
     MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
     BrowserAnimationsModule
   ],
   providers: [],
